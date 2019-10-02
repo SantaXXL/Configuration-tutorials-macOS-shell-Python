480f99549b57ef3ceec700d3786f8296c23758d1
Dominik Mańkowski <mankowski.dominik@gmail.com>
2019-10-02 14:12:48 +0200

    Update pretty-zsh.sh

--- pretty-zsh.sh
+++ pretty-zsh.sh
@@ -57,7 +57,7 @@ sudo sh -c "echo $(which zsh) >> /etc/shells"
 chsh -s $(which zsh)
 
 # install oh-my-zsh and syntax-highlighting
-echo "Installing oh-my-zsh (switching shell to zsh)"
+echo "Installing oh-my-zsh"
 yes n | zsh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
 
