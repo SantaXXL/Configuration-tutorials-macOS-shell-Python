c890bf2a0e2f88a4c41bc75e70648fe13db37f5b
Dominik Mańkowski <mankowski.dominik@gmail.com>
2019-10-02 19:49:36 +0200

    Create install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# quickly install all the programs I use after factory reset
+
+# install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
+
+# install linuxify - replaces freeBSD unix tools with GNU ones
+brew install git
+git clone https://github.com/fabiomaia/linuxify.git
+./linuxify/linuxify install
+
+# install and configure shell
+curl -O https://raw.githubusercontent.com/SantaXXL/Configuration-tutorials-macOS-shell-Python-/master/pretty-zsh-macos.sh
+chmod +x pretty-zsh-macos.sh
+./pretty-zsh-macos.sh
+rm -f pretty-zsh-macos.sh
+echo '. ~/.linuxify' >> ~/.zshrc
+
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json visual-studio-code vlc transmission pdf-expert qnapi fluor spotify google-chrome adguard 
+
+brew tap colindean/fonts-nonfree 
+brew cask install font-microsoft-office
+
+brew tap homebrew/cask-drivers
+brew cask install logitech-options
+
